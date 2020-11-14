4a0009e8a170bd93933523fbcc6a1b11dd1d7fba
Nikolas Stefano <nz.stefano@gmail.com>
2020-11-14 16:25:16 +0700

    Adding cors

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = 3000;
 const routes = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 app.use(errorHandler);
 
-// app.listen(port, () => {
-//   console.log(`App running at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
 
 module.exports = app;
